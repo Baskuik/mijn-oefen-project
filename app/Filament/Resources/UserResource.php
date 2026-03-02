d812d35a100e18fbc9b6203f38b7eb37a4c5eeca
Baskuik <97105236@st.deltion.nl>
2026-03-02 09:52:50 +0100

    php artisan serve werkt niet

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -4,9 +4,9 @@ namespace App\Filament\Resources;
 
 use App\Filament\Resources\UserResource\Pages;
 use App\Models\User;
-use Filament\Forms\Form; // VERANDERD: In v3 gebruik je Form, niet Schema
 use Filament\Resources\Resource;
-use Filament\Tables\Table;
+use Filament\Schemas\Schema; 
+use Filament\Tables\Table;   
 use Filament\Forms\Components\TextInput;
 use Filament\Tables\Columns\TextColumn;
 
@@ -14,19 +14,21 @@ class UserResource extends Resource
 {
     protected static ?string $model = User::class;
 
-    // FIX 1: Verwijder de '?string' type hint of gebruik de brede hint
-    protected static ?string $navigationIcon = 'heroicon-o-users'; 
-    // Als bovenstaande nog steeds een error geeft, gebruik: protected static $navigationIcon = 'heroicon-o-users';
+    protected static ?string $navigationIcon = 'heroicon-o-users';
 
     protected static ?string $slug = 'users';
 
-    // FIX 2: De functie moet 'form(Form $form)' zijn, niet 'Schema'
-    public static function form(Form $form): Form
+    public static function form(Schema $schema): Schema
     {
-        return $form
-            ->schema([ // In v3 gebruik je ->schema() in plaats van ->components()
-                TextInput::make('name')->label('Naam')->required(),
-                TextInput::make('email')->label('E-mail')->email()->required(),
+        return $schema
+            ->components([
+                TextInput::make('name')
+                    ->label('Naam')
+                    ->required(),
+                TextInput::make('email')
+                    ->label('E-mail')
+                    ->email()
+                    ->required(),
             ]);
     }
 
@@ -34,16 +36,22 @@ class UserResource extends Resource
     {
         return $table
             ->columns([
-                TextColumn::make('name')->label('Naam')->sortable()->searchable(),
-                TextColumn::make('email')->label('E-mail')->sortable()->searchable(),
+                TextColumn::make('name')
+                    ->label('Naam')
+                    ->sortable()
+                    ->searchable(),
+                TextColumn::make('email')
+                    ->label('E-mail')
+                    ->sortable()
+                    ->searchable(),
             ])
             ->actions([
+                // We dwingen PHP om hier te kijken:
                 \Filament\Tables\Actions\EditAction::make(),
             ])
             ->bulkActions([
-                \Filament\Tables\Actions\BulkActionGroup::make([
-                    \Filament\Tables\Actions\DeleteBulkAction::make(),
-                ]),
+                // We dwingen PHP om hier te kijken:
+                \Filament\Tables\Actions\DeleteBulkAction::make(),
             ]);
     }
 
