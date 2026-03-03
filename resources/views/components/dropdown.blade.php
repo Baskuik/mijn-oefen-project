c1eb1d221de97fc08d9b3deb870bc75d6f5f9a6a
Baskuik <97105236@st.deltion.nl>
2026-03-03 15:23:33 +0100

    Ticket#6 (adding page to view account information)

--- resources/views/components/dropdown.blade.php
+++ resources/views/components/dropdown.blade.php
@@ -1,11 +1,43 @@
-        @props(['active'])
+@props(['align' => 'right', 'width' => '48', 'contentClasses' => 'py-1 bg-white'])
 
-    @php
-$classes = ($active ?? false)
-            ? 'block w-full px-4 py-2 text-start text-sm leading-5 text-gray-700 bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out'
-            : 'block w-full px-4 py-2 text-start text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out';
-@endphp
+@php
+switch ($align) {
+    case 'left':
+        $alignmentClasses = 'ltr:origin-top-left rtl:origin-top-right start-0';
+        break;
+    case 'top':
+        $alignmentClasses = 'origin-top';
+        break;
+    case 'right':
+    default:
+        $alignmentClasses = 'ltr:origin-top-right rtl:origin-top-left end-0';
+        break;
+}
 
-<a {{ $attributes->merge(['class' => $classes]) }}>
-    {{ $slot }}
-</a>
+switch ($width) {
+    case '48':
+        $width = 'w-48';
+        break;
+}
+@endphp  
+
+<div class="relative" x-data="{ open: false }" `@click.outside`="open = false" `@close.stop`="open = false">
+    <div `@click`="open = ! open">
+        {{ $trigger }}
+    </div>
+
+    <div x-show="open"
+            x-transition:enter="transition ease-out duration-200"
+            x-transition:enter-start="opacity-0 scale-95"
+            x-transition:enter-end="opacity-100 scale-100"
+            x-transition:leave="transition ease-in duration-75"
+            x-transition:leave-start="opacity-100 scale-100"
+            x-transition:leave-end="opacity-0 scale-95"
+            class="absolute z-50 mt-2 {{ $width }} rounded-md shadow-lg {{ $alignmentClasses }}"
+            style="display: none;"
+            `@click`="open = false">
+        <div class="rounded-md ring-1 ring-black ring-opacity-5 {{ $contentClasses }}">
+            {{ $content }}
+        </div>
+    </div>
+</div>
